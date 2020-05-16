5cdfaff2aebf74e5ae864ea51ace50b7b3221b06
Patrick Garvey <patrick.garvey@gmx.de>
2020-05-16 18:38:09 +0200

    exercise1-3

--- Chapter1/Fahrenheit-Celsius_table.c
+++ Chapter1/Fahrenheit-Celsius_table.c
@@ -4,11 +4,14 @@ int main()
 {
   float fahr, celsius;
   int lower, upper, step;
+  char heading[];
 
   lower = 0;      /* lower limit of temperature table */
   upper = 300;    /* upper limit */
   step = 20;      /* step size */
+  heading = "Fahrenheit-Celsius_table";
 
+  printf("%s\n", heading);
   fahr = lower;
   while (fahr <= upper) {
     celsius = (5.0/9.0) * (fahr-32.0);
