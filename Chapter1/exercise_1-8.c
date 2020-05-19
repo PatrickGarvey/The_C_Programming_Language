6dbb302f06d6b1476e562e74f2c06313450de8fe
Patrick Garvey <patrick.garvey@gmx.de>
2020-05-19 11:33:25 +0200

    exercise 1-8

---
+++ Chapter1/exercise_1-8.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+  int c, nl, nb, nt;
+
+  nl = 0;
+  nb = 0;
+  nt = 0;
+
+  while ((c = getchar()) != EOF)
+    if (c == '\n')
+      ++nl;
+    else if (c == '\t')
+      ++nt;
+    else if (c == ' ')
+      ++nb;
+
+    printf("newline: %d\ntabs: %d\nblanks: %d\n", nl, nt, nb);
+  return 0;
+}
